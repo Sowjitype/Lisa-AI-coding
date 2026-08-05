39348a5b7008723c644a61b2a778f8fd05d580ca
Sowjitype <107684487+Sowjitype@users.noreply.github.com>
2026-08-05 16:40:45 +0800

    Add files via upload

---
+++ L21 hw.py
@@ -0,0 +1,29 @@
+import requests
+
+# Random Technology Facts API
+url = "https://uselessfacts.jsph.pl/random.json?language=en"
+
+# Function to get a random technology fact
+def get_random_technology_fact():
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        print("Random Technology Fact:")
+        print(f"Did you know? {data['text']}")
+        print("\n")
+    else:
+        print("Failed to retrieve a random technology fact.")
+
+# Main loop to interact with the user
+while True:
+    key = input("Press Enter to get a random technology fact or type 'q' to quit: ")
+
+    if key.lower() == 'q':
+        print("Exiting the program...")
+        print("Goodbye!")
+        break
+    elif key == 'm':        
+        get_random_technology_fact()
+    else:
+        print("Invalid input. Please press Enter to get a fact or 'q' to quit.")
